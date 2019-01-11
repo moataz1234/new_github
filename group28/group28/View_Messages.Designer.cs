fd3b665b69085d1d17e551d7601f3d9396763897
odai <odaiwa@ac.sce.ac.il>
2019-01-11 13:25:52 +0200

    fix datagridview for messages

--- group28/group28/View_Messages.Designer.cs
+++ group28/group28/View_Messages.Designer.cs
@@ -38,11 +38,12 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(136, 126);
+            this.dataGridView1.Location = new System.Drawing.Point(90, 112);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(605, 312);
+            this.dataGridView1.Size = new System.Drawing.Size(651, 326);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
